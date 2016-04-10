8cf4dc16672f795a7fb4ef934237bdfa676d2e2e
Faline10 <fawn.be@gmail.com>
2016-04-09 17:55:07 -0700

    Get to Hello World status

--- App.jsx
+++ App.jsx
@@ -1,3 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Layout = require('./components/Layout.jsx');
+// var Layout = require('./components/Layout.jsx');
+
+ReactDOM.render(
+  <h1 style={{backgroundColor: 'red'}} className="someClass">Hello, world!</h1>,
+  document.getElementById('app')
+);
